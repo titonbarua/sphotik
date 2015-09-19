4861a2e7daba44193d025396af171b423567cbd5
Titon Barua <titanix88@gmail.com>
2015-09-20 01:07:10 +0600

    Release 1.0 | Python setuptool based packaging complete.

---
+++ README.md
@@ -0,0 +1,8 @@
+# Sphotiklib: Avro-like Bangla phonetic parser in Python
+
+This is the core library used by 'Sphotik', an IBus input method engine
+for Linux, ( yet to be ) written in Python.
+
+# Usage
+
+The usage is best explained with some code. Please look at the `example.py`.
---
+++ example.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+from sphotiklib.parser import Parser
+from sphotiklib.ruleparser import Rule
+
+# We will use the builtin ruleset 'avro'.
+p = Parser(Rule('avro'))
+
+# Input made as a big string.
+p.insert('kichu manuSh sbadhInvabe soman morzada')
+
+# Input made in small-chunks.
+for c in ' ebong odhikar niye jonmogrohon kore.':
+    p.insert(c)
+
+# Print parsed text.
+print(p.text)
+
+# cursor attribute can be used to insert chars at arbitrary positions.
+# Cursor is only meaningful in converted text domain.
+p.cursor = 5
+
+# delete method can be used to delete text at cursor. Positive step means
+# deletion from front. Negative means deletion from back.
+p.delete(-5)
+print(p.text)
+
+p.insert('somosto ')
+print(p.text)
